3503e41eb4aa159d670be50a58a824413e9b990f
JrmyDlms <173142512+JrmyDlms@users.noreply.github.com>
2024-08-21 11:16:28 +0200

    form

--- app/public/wp-content/themes/hello-elementor-child/functions.php
+++ app/public/wp-content/themes/hello-elementor-child/functions.php
@@ -1,7 +1,22 @@
 <?php
-
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    // Charger les styles du thème parent
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 
+    // Charger les styles du thème enfant
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), wp_get_theme()->get('Version'));
 }
+
+// Supprimer les styles spécifiques du thème parent
+function remove_parent_styles() {
+    wp_dequeue_style('hello-elementor'); // Remplacez 'hello-elementor-css' par l'ID correct du style à supprimer
+}
+
+// Ajoutez l'action pour supprimer les styles du parent
+add_action('wp_enqueue_scripts', 'remove_parent_styles', 20);
+
+// Ajoutez l'action pour charger les styles
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+?>
+
+
